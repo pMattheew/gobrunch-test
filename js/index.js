4a97afaff2f5509a45ca94ed0e6e20ce789f9375
Patrick M <patrickmatias.silva@hotmail.com>
2024-04-05 00:55:57 -0300

    feat: added interactivity with ejs

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log("Hello, world!")
+const listItem = `<li>
+<span><%= number %></span>
+</li>`
+
+document.addEventListener("DOMContentLoaded", () => {
+  while (!ejs) {
+    setTimeout(() => null, 500)
+  }
+  const inputElement = document.querySelector('input[type="number"]')
+  const listElement = document.querySelector("ol")
+
+  inputElement.addEventListener("input", event => {
+    listElement.innerHTML = ""
+    const quantity = event.target.value
+    for (let i = 0; i < quantity; i++) {
+      const rendered = ejs.render(listItem, { number: i + 1 })
+      listElement.innerHTML += rendered
+    }
+  })
+})
